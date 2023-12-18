a77d6a49a71db7faf74561bbc4145831a6e48bd8
lauropjdutra <lauropjdutra@gmail.com>
2023-12-18 13:41:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// Elementos do DOM
+const button = document.querySelector(".draw-button");
+const input = document.querySelector(".input-max");
+
+// INÍCIO DA FUNÇÃO CALC
+function calc() {
+  const min = Math.ceil(document.querySelector(".input-min").value)
+  const max = Math.floor(document.querySelector(".input-max").value)
+  const result = Math.floor(Math.random() * (max - min + 1) ) + min
+  return result
+}
+
+function showResult() {
+  const box = document.querySelector(".result-box")
+  const background = document.querySelector(".gradient")
+  const display = document.querySelector(".result-display")
+  box.style.display = "flex"
+  box.style.opacity = "1"
+  background.style.display = "block"
+  background.style.opacity = "1"
+  display.innerHTML = calc()
+}
+
+button.addEventListener("click", showResult);
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    button.click();
+  }
+});
